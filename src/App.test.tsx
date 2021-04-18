d01a5d48bf663125774a6470723a35e2ec86812d
Mark Marincek <mark.marincek@styliff.com>
2021-04-19 01:30:40 +0200

    Fixed the test..

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Git Repositories', () => {
+  const observe = jest.fn();
+
+  // @ts-ignore
+  window.IntersectionObserver = jest.fn(function () {
+    // @ts-ignore
+    this.observe = observe;
+  });
+
+  render(
+    <MockedProvider mocks={[]} addTypename={false}>
+      <App />
+    </MockedProvider>
+  );
+  const title = screen.getByText(/Git Repositories/i);
+  expect(title).toBeInTheDocument();
 });
