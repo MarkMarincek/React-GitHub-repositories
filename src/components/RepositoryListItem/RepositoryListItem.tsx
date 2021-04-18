17b015b10d444c87d41024dad9b7b374f982017e
Mark Marincek <mark.marincek@styliff.com>
2021-04-18 23:57:10 +0200

    Added link to the github repositories.

--- src/components/RepositoryListItem/RepositoryListItem.tsx
+++ src/components/RepositoryListItem/RepositoryListItem.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { List } from 'antd';
+import { Button, List } from 'antd';
 import { Repository } from 'models/interfaces';
+import './RepositoryListItem.style.css';
 
 interface RepositoryListItemProps {
   repository: Repository;
 }
 
 export default function RepositoryListItem({ repository }: RepositoryListItemProps) {
-  const { name, stargazerCount, forkCount } = repository;
+  const { name, stargazerCount, forkCount, url } = repository;
   return (
     <List.Item>
-      {name} - 🌟 {stargazerCount} - 🍴 {forkCount}
+      <a href={url} className="list__item">
+        {name} - 🌟 {stargazerCount} - 🍴 {forkCount}
+      </a>
     </List.Item>
   );
 }
