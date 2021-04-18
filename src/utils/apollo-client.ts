c512f61867ec9395c79decbb50b73c94bdeb144c
Mark Marincek <mark.marincek@styliff.com>
2021-04-18 23:44:01 +0200

    Added merge function for search query results to enable "fetchMore" function.

--- src/utils/apollo-client.ts
+++ src/utils/apollo-client.ts
@@ -1,10 +1,32 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { API_URL, GIT_ACCESS_TOKEN } from 'config';
+import { Repository, Search } from 'models/interfaces';
 
 export default new ApolloClient({
   uri: API_URL,
   headers: {
     Authorization: `bearer ${GIT_ACCESS_TOKEN}`,
   },
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          search: {
+            keyArgs: ['query'],
+            merge(
+              existing?: Search<Repository>,
+              incoming?: Search<Repository>
+            ): Search<Repository> | undefined {
+              if (!existing) return incoming;
+              if (!incoming) return existing;
+              return {
+                ...incoming,
+                nodes: [...(existing?.nodes ?? []), ...(incoming?.nodes ?? [])],
+              };
+            },
+          },
+        },
+      },
+    },
+  }),
 });
